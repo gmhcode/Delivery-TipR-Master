8b90570f182ec82e3070ee82dcfafcd3bb19a962
Greg Hughes <dcgg_101@hotmail.com>
2020-04-02 11:14:32 -0600

    created material components prototype

---
+++ Delivery TipR/BlahViewController.swift
@@ -0,0 +1,37 @@
+//
+//  BlahViewController.swift
+//  Delivery TipR
+//
+//  Created by Greg Hughes on 4/2/20.
+//  Copyright © 2020 Greg Hughes. All rights reserved.
+//
+
+import UIKit
+import MaterialComponents
+
+class BlahViewController: UIViewController {
+
+    @IBOutlet weak var helloTextField: MDCTextField!
+    @IBOutlet weak var blahTextField: MDCTextField!
+    
+    var nameController: MDCTextInputControllerOutlined?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        nameController = MDCTextInputControllerOutlined(textInput: helloTextField)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
