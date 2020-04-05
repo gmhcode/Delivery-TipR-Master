1fccd5ae6abaf601db82981e53afbc154c5dcaa1
Greg Hughes <dcgg_101@hotmail.com>
2020-04-04 20:00:24 -0600

    forgot password vc

---
+++ Delivery TipR/ViewControllers/SignIn/ForgotPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ForgotPasswordViewController.swift
+//  Delivery TipR
+//
+//  Created by Greg Hughes on 4/4/20.
+//  Copyright © 2020 Greg Hughes. All rights reserved.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
