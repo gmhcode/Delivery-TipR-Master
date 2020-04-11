67022572a4d1de8c1259e5ecbe7249ce11e71189
Greg Hughes <dcgg_101@hotmail.com>
2020-04-10 21:37:47 -0600

    working on customer detail view controller

---
+++ Delivery TipR/ViewControllers/CustomerDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CustomerDetailViewController.swift
+//  Delivery TipR
+//
+//  Created by Greg Hughes on 4/10/20.
+//  Copyright © 2020 Greg Hughes. All rights reserved.
+//
+
+import UIKit
+
+class CustomerDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
