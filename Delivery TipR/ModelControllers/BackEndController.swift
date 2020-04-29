09e4584829b12d61db3b793682df6b65e3587908
Greg Hughes <dcgg_101@hotmail.com>
2020-04-29 17:37:55 -0600

    added post to aws functionality for delivery

---
+++ Delivery TipR/ModelControllers/BackEndController.swift
@@ -0,0 +1,17 @@
+//
+//  BackEndController.swift
+//  Delivery TipR
+//
+//  Created by Greg Hughes on 4/29/20.
+//  Copyright © 2020 Greg Hughes. All rights reserved.
+//
+
+import Foundation
+struct BackEndController {
+    
+    static let postDeliveryUrl = URL(string: !
+    
+    
+    
+    
+}
